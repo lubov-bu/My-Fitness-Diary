fa12d419051ac07710830fcb08354066a38017f5
Lubov <lb.librab14@gmail.com>
2022-05-23 17:05:36 +0300

    Initial Commit

---
+++ My Fitness Diary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My Fitness Diary
+//
+//  Created by Lubov Burbygina on 23.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

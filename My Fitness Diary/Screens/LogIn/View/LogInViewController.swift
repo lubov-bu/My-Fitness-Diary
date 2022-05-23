f42dfe32a692d620fdda84254403e81864a0e497
Lubov <lb.librab14@gmail.com>
2022-05-23 23:52:15 +0300

    logIn screen designed

---
+++ My Fitness Diary/Screens/LogIn/View/LogInViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  My Fitness Diary
+//
+//  Created by Lubov Burbygina on 23.05.2022.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var mailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var logInButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    @IBAction func authorize(_ sender: UIButton) {
+    }
+    
+    @IBAction func register(_ sender: UIButton) {
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+           self.view.endEditing(true)
+       }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        
+    }
+
+
+}
+

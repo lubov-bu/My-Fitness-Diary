ceb982d4877796f0afe353a485d03c3371233596
Lubov <lb.librab14@gmail.com>
2022-05-26 18:52:30 +0300

    add sign up view controller

---
+++ My Fitness Diary/Screens/Main/View/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  My Fitness Diary
+//
+//  Created by Lubov Burbygina on 26.05.2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
---
+++ My Fitness Diary/Screens/SignUp/View/SignUpViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SignUpViewController.swift
+//  My Fitness Diary
+//
+//  Created by Lubov Burbygina on 26.05.2022.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+    
+    
+    @IBOutlet weak var createLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mailTextField: UITextField!
+    @IBOutlet weak var phoneTextField: UITextField!
+    @IBOutlet weak var birthdayTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var createProfileButton: UIButton!
+    
+    @IBAction func createProfile(_ sender: UIButton) {
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+           self.view.endEditing(true)
+       }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+}

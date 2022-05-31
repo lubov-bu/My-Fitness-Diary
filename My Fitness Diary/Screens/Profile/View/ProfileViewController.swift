713fe8f6ad28d76c8e16435e94ca4b44c8d22fed
Lubov <lb.librab14@gmail.com>
2022-05-31 15:37:38 +0300

    changing style

---
+++ My Fitness Diary/Screens/Profile/View/ProfileViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileViewController.swift
+//  My Fitness Diary
+//
+//  Created by Lubov Burbygina on 31.05.2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
---
+++ My Fitness Diary/Screens/Statistics/View/StatisticsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  StatisticsViewController.swift
+//  My Fitness Diary
+//
+//  Created by Lubov Burbygina on 31.05.2022.
+//
+
+import UIKit
+
+class StatisticsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}

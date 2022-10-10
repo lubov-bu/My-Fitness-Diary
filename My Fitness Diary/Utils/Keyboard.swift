2480fabb266f75a86dcff89a4f25e1eec4de1daa
Lubov <lb.librab14@gmail.com>
2022-10-10 22:27:11 +0300

    core data

--- My Fitness Diary/Utils/Keyboard.swift
+++ My Fitness Diary/Utils/Keyboard.swift
@@ -8,24 +8,3 @@
 import Foundation
 import UIKit
 
-// This extension moves content up when keyboard appears
-
-extension UIViewController {
-    
-    
-    @objc func keyboardWillShow(notification: NSNotification) {
-        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
-            if self.view.frame.origin.y == 0 {
-                self.view.frame.origin.y -= keyboardSize.height
-            }
-        }
-    }
-
-    @objc func keyboardWillHide(notification: NSNotification) {
-        if self.view.frame.origin.y != 0 {
-            self.view.frame.origin.y = 0
-        }
-    }
-    
-}
-
